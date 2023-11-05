66f750cf91221ecb859966e257a7c2224d90b838
alextuchkov <alextychkoff@gmail.com>
2023-11-05 13:34:38 +0200

    added basic ListItem CRUD along with some ui changes

--- views.py
+++ views.py
@@ -49,8 +49,7 @@ def signin():
         password = request.form.get("password")
 
         # Check if a valid user with the provided email exists
-        # user = User.query.filter_by(email=email).first()
-        user = db.session.query(User).filter_by(email=email).first()
+        user = session.query(User).filter_by(email=email).first()
 
         if user and check_password_hash(user.password_hash, password):
             user.authenticated = True
@@ -71,41 +70,11 @@ def logout():
     return redirect(url_for("index"))
 
 
-# @app.route("/new", methods=["GET", "POST"])
-# @login_required
-# def new():
-#     if request.method == "POST":
-#         deadline_date = datetime.strptime(
-#             request.form.get("deadline"), "%Y-%m-%d"
-#         ).date()
-
-#         new_list = List(
-#             name=request.form.get("list_name"),
-#             description=request.form.get("description"),
-#             deadline=deadline_date,
-#             owner=current_user.id,
-#         )
-
-#         session.add(new_list)
-#         session.commit()
-#         session.close()
-
-#     return render_template("create.html")
-
-
 @app.route("/new", methods=["GET", "POST"])
 @login_required
 def new():
     if request.method == "POST":
         try:
-            # data = request.get_json()  # Parse the JSON data from the client
-            # # Access data using data['key']
-            # list_name = data["listName"]
-            # deadline = data["deadline"]
-            # list_description = data["listDescription"]
-            # items = data["items"]
-
-            # Process and store the data as needed (e.g., in a database)
             new_list_name = request.form.get("list_name")
             new_list_descrp = request.form.get("list_description")
             deadline = datetime.strptime(
@@ -128,24 +97,27 @@ def new():
             return redirect(url_for("edit", id=newly_created_list.id))
 
         except Exception as e:
+            flash(f"An error occurred while adding list item: {str(e)}", "error")
             return "Some Error"
+
     return render_template("create.html")
 
 
 @app.route("/edit/<int:id>", methods=["GET", "POST"])
 @login_required
 def edit(id):
-    list_item = session.query(List).filter_by(id=id).first()
-    list_single_item = session.query(ListItem).filter_by(list=list_item.id).all()
+    page_title = "Edit list"
+    list = session.query(List).filter_by(id=id).first()
+    list_items = session.query(ListItem).filter_by(list=list.id).all()
 
     if request.method == "POST":
         deadline_date = datetime.strptime(
             request.form.get("deadline"), "%Y-%m-%d"
         ).date()
 
-        list_item.name = request.form.get("name")
-        list_item.description = request.form.get("description")
-        list_item.deadline = deadline_date
+        list.name = request.form.get("name")
+        list.description = request.form.get("description")
+        list.deadline = deadline_date
 
         try:
             session.commit()
@@ -154,10 +126,10 @@ def edit(id):
             session.rollback()
             flash(f"An error occurred while updating list details: {str(e)}", "error")
 
-        return redirect(url_for("edit", id=list_item.id))
+        return redirect(url_for("edit", id=list.id))
 
     return render_template(
-        "edit.html", list=list_item, list_signle_item=list_single_item
+        "edit.html", list=list, list_items=list_items, title=page_title
     )
 
 
@@ -176,7 +148,7 @@ def add_list_item():
             item_description=item_description,
             price=item_price,
             url=item_url,
-            list=ref_id
+            list=ref_id,
         )
         try:
             session.add(new_list_item)
@@ -187,16 +159,62 @@ def add_list_item():
             flash(f"An error occurred while adding list item: {str(e)}", "error")
 
         return redirect(url_for("edit", id=ref_id))
-      
+
+
+@app.route("/update_list_item", methods=["GET", "POST"])
+@login_required
+def update_list_item():
+    if request.method == "POST":
+        item_id = request.form.get("item-id")
+        item_name = request.form.get("item-name")
+        item_description = request.form.get("item-description")
+        item_price = request.form.get("item-price")
+        item_url = request.form.get("item-link")
+        ref_id = request.form.get("ref-id")
+
+        list_item = session.query(ListItem).filter_by(id=item_id).first()
+
+        if list_item is not None:
+            list_item.item_name = item_name
+            list_item.item_description = item_description
+            list_item.price = item_price
+            list_item.url = item_url
+
+        try:
+            session.commit()
+            flash("List Item updated successfully.", "success")
+        except Exception as e:
+            session.rollback()
+            flash(f"An error occurred while updating list item: {str(e)}", "error")
+
+        return redirect(url_for("edit", id=ref_id))
+
+
+@app.route("/delete_list_item/<int:id>", methods=["GET", "POST"])
+@login_required
+def delete_list_item(id):
+    list_item = session.query(ListItem).filter_by(id=id).first()
+    if list_item:
+        try:
+            session.delete(list_item)
+            session.commit()
+            flash("List item deleted successfully.", "success")
+        except Exception as e:
+            session.rollback()
+            flash(f"An error occurred while deleting the list item: {str(e)}", "error")
+    else:
+        flash("List item not found.", "error")
+
+    return redirect(url_for("edit", id=list_item.list))
 
 
 @app.route("/delete/<int:id>", methods=["GET", "POST"])
 @login_required
 def delete(id):
-    list_item = session.query(List).filter_by(id=id).first()
-    if list_item:
+    list = session.query(List).filter_by(id=id).first()
+    if list:
         try:
-            session.delete(list_item)
+            session.delete(list)
             session.commit()
             flash("List item deleted successfully.", "success")
         except Exception as e:
@@ -210,8 +228,10 @@ def delete(id):
 
 @app.route("/list/<int:id>")
 def list(id):
-    list_item = session.query(List).filter_by(id=id).first()
-    return render_template("list.html", list=list_item)
+    list = session.query(List).filter_by(id=id).first()
+    list_items = session.query(ListItem).filter_by(list=list.id).all()
+
+    return render_template("list.html", list=list, list_items=list_items)
 
 
 @app.route("/dashboard", methods=["GET", "POST"])
@@ -240,10 +260,3 @@ def dashboard():
         return redirect(url_for("dashboard"))
 
     return render_template("dashboard.html", lists=lists)
-
-
-@app.route("/profile/<int:id>")
-def profile(id):
-    user = User.query.filter_by(id=id).first()
-    lists = session.query(List).filter_by(owner=user.id).all()
-    return render_template("profile.html", user=user, lists=lists)
